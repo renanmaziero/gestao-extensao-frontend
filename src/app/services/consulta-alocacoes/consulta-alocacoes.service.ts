63ac91157916c994bc5ddf5280db6af939760455
Renan <renanfmaziero@hotmail.com>
2023-03-13 16:14:36 -0300

    implementado service funcao consultarAlocacoes

--- src/app/services/consulta-alocacoes/consulta-alocacoes.service.ts
+++ src/app/services/consulta-alocacoes/consulta-alocacoes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { TotalHorasAprovadas } from 'src/app/models/total-horas-aprovadas.model';
 import { ConsultaAlocacoes } from 'src/app/models/consulta-alocacoes.model';
 
 @Injectable({
@@ -13,8 +14,12 @@ export class ConsultaAlocacoesService {
 
   constructor(private http: HttpClient) {}
 
-  consultarAlocacoes(docenteId: number): Observable<ConsultaAlocacoes>{
-    console.log(this.baseUrl + '/consultar/' + docenteId);
-    return this.http.get<ConsultaAlocacoes>(this.baseUrl + '/consultar/' + docenteId)
+  totalHorasAprovadas(docenteId: number): Observable<TotalHorasAprovadas>{
+    return this.http.get<TotalHorasAprovadas>(this.baseUrl + '/total-horas-aprovadas/' + docenteId)
+  }
+
+  consultarAlocacoes(docenteId: number, semestre: number, ano: number): Observable<ConsultaAlocacoes>{
+    console.log(this.baseUrl + '/consultar-alocacoes/' + docenteId + '/' + semestre + '/' + ano);
+    return this.http.get<ConsultaAlocacoes>(this.baseUrl + '/consultar-alocacoes/' + docenteId + '/' + semestre + '/' + ano)
   }
 }
