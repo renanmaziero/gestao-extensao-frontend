a92fa62e6bd317e0432588454d265442ce148d93
Renan <renanfmaziero@hotmail.com>
2023-03-23 01:41:33 -0300

    add dialog horas excedeu

---
+++ src/app/components/formulario/dialog/dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<DialogComponent>) { }
+
+  public semestre: number;
+  public ano: number;
+  public tipo: string;
+
+  ngOnInit(): void {
+  }
+
+}
